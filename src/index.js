b851b91d6a1f3e9ef168fdeea8c6ae3b2cee223f
mr.funkster <mr.funksters@gmail.com>
2021-02-13 00:19:43 +0200

    remove redux devtools

--- src/index.js
+++ src/index.js
@@ -16,9 +16,8 @@ import thunk from 'redux-thunk';
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ),
+    applyMiddleware(thunk)
+  )
 );
 
 ReactDOM.render(
